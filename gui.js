6bfcb041dd42ed9c6dd6ce5325d1d650b08a3899
Jolle Lont <jolle.lont@malengo.nl>
2015-07-22 16:38:24 +0200

    initialize digit boxes in gui

--- gui.js
+++ gui.js
@@ -1,5 +1,6 @@
 var Gui = function(dispatcher) {
 	var elements = Immutable.Map([['start', $('#start')], ['kluis', $('#kluis')]]);
+	var numDigits = 0;
 	
 	var hideAll = function() {
 		elements.map(function(x) {x.hide();});
@@ -9,14 +10,21 @@ var Gui = function(dispatcher) {
 		hideAll();
 		elements.get(elem).show();
 	};
-	
-	
-	
+
 	var onNumber = function(message) {
-		$('#kluis').html('test');
+		var update = function(n) {
+			$($('.digitBox')[n]).html(message.current.get(n));
+		};
+		Immutable.Range(0, numDigits).forEach(update);
 	};
 	
-	var initialize = function() {
+	var initialize = function(message) {
+		numDigits = message.lengthOfCode;
+		var digitBox = "<div class='digitBox'></div>";
+		Immutable
+			.Repeat(digitBox, numDigits)
+			.forEach(function(x) { $('#kluis').append(x); });
+		
 		show('kluis');
 		dispatcher.Subscribe('Number', onNumber);
 	};
