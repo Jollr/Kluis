91490779f916d8ba915f2594dfd1a25f1c1033fc
Jolle Lont <jolle.lont@malengo.nl>
2015-07-22 14:50:39 +0200

    dispatcher

---
+++ dispatcher.js
@@ -0,0 +1,17 @@
+var Dispatcher = function() {
+	var subscriptions = new Immutable.Map();
+
+	this.Subscribe = function(name, callback) {
+		var added = subscriptions
+			.get(name, new Immutable.List())
+			.push(callback);
+			
+		subscriptions = subscriptions.merge(Immutable.Map({name: added}));
+	};
+	
+	this.Publish = function(name, message) {
+		subscriptions
+			.get(name, new Immutable.List())
+			.map(function (subscribedFunction) { subscribedFunction(message); });
+	};
+};
