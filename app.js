e76b525993fd2074a063dfef45550d5a60d9737a
Jolle Lont <jolle.lont@malengo.nl>
2015-07-22 15:21:02 +0200

    register number inputs

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 $(function() {
 	var dispatcher = new Dispatcher();
 	var gui = new Gui(dispatcher);
+	var input = new Input(dispatcher);
+	
 	dispatcher.Publish('Initialize', {});
 });
---
+++ input.js
@@ -0,0 +1,21 @@
+﻿var Input = function(dispatcher) {
+	var initialize = function() {
+		document.onkeydown = function(e) {
+			if (e.keyCode == 116) { // f5
+				return true;
+			}
+			
+			if (e.keyCode >= 96 && e.keyCode <= 105) {
+				dispatcher.Publish('numberTyped', { number: e.keyCode - 96 });
+			}
+			
+			if (e.keyCode == 8) { // backspace
+				Dispatcher.Publish('backspace', { });
+			}
+			
+			return false;
+		};
+	};
+	
+	dispatcher.Subscribe('Initialize', initialize);
+};
